f3785baae2ab06231a28a27739ebfc7ca2906c8a
Umbreella <draginvadim@gmail.com>
2023-05-20 12:41:45 +0600

    Change Access LifeTime

--- medic_madskill/settings.py
+++ medic_madskill/settings.py
@@ -153,7 +153,7 @@ SWAGGER_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
